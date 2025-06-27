7cc056264dd750ef6312b029a59656716bad9b5c
ashkan-cell-91 <a.adabi2022@gmail.com>
2025-06-28 00:56:33 +0400

    update DjangoBlog folder

--- articles/views.py
+++ articles/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, HttpResponse
 from . import models
 
 def articles_list(request):
-    articles = models.Article.objects.all().order_by('date')
+    articles = models.Article.objects.all().order_by('-date')
     args = {'articles': articles}
     return render(request, 'articles/articleslist.html', args)
 def articles_details(request, slug):
-    return HttpResponse(slug)
+    #return HttpResponse(slug)
+    article = models.Article.objects.get(slug=slug)
+    args = {'article': article}
+    return render(request, 'articles/article_details.html', args)
